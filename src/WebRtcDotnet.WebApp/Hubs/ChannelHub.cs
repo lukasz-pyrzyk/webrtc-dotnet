976c5a6949b1ef4a1a73bb6b99e50de23fdd9ef2
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2022-11-02 13:31:06 +0100

    Removing empty rooms

--- src/WebRtcDotnet.WebApp/Hubs/ChannelHub.cs
+++ src/WebRtcDotnet.WebApp/Hubs/ChannelHub.cs
@@ -21,10 +21,33 @@ public class ChannelHub : Hub
         return base.OnConnectedAsync();
     }
 
-    public override Task OnDisconnectedAsync(Exception exception)
+    public override async Task OnDisconnectedAsync(Exception exception)
     {
         _logger.LogInformation("Client {clientId} disconnected", Context.ConnectionId);
-        return base.OnDisconnectedAsync(exception);
+        using (await Mutex.LockAsync())
+        {
+            var changed = false;
+            foreach (var room in Rooms.Where(x => x.Participants.Contains(Context.ConnectionId)))
+            {
+                _logger.LogDebug("Removing participant {clientId} from room {roomId}", Context.ConnectionId, room.Id);
+                room.Participants.Remove(Context.ConnectionId);
+
+                if (room.Participants.Count == 0)
+                {
+                    _logger.LogInformation("Removing room {roomId}", room.Id);
+                    Rooms.Remove(room);
+                }
+
+                changed = true;
+            }
+
+            if (changed)
+            {
+                await RaiseRoomsUpdated();
+            }
+        }
+
+        await base.OnDisconnectedAsync(exception);
     }
 
     public async Task CreateRoom(string roomName)
@@ -41,7 +64,7 @@ public class ChannelHub : Hub
 
             var room = new RoomDTO((Rooms.Count + 1).ToString(), roomName, new List<string>());
             Rooms.Add(room);
-            await Clients.All.SendAsync("RoomsUpdated", Rooms);
+            await RaiseRoomsUpdated();
         }
     }
 
@@ -63,12 +86,33 @@ public class ChannelHub : Hub
             var isFirstClient = room.Participants.Count == 1;
             await Clients.Caller.SendAsync("Joined", roomId, isFirstClient);
             _logger.LogInformation("Participant {clientId} joined the room {roomId}", Context.ConnectionId, roomId);
+            await RaiseRoomsUpdated();
 
             if (room.Participants.Count == 2)
             {
                 await Clients.Group(roomId).SendAsync("Ready");
                 _logger.LogInformation("Room {roomId} have 2 participants, sending ready information", roomId);
             }
+        }
+    }
+
+    public async Task Leave(string roomId)
+    {
+        using (await Mutex.LockAsync())
+        {
+            var room = Rooms.SingleOrDefault(x => x.Id == roomId);
+            if (room is null)
+            {
+                _logger.LogError("Unable to find room {roomId}", roomId);
+                return;
+            }
+
+            _logger.LogDebug("Removing participant {clientId} from room {roomId}", Context.ConnectionId, roomId);
+            room.Participants.Remove(Context.ConnectionId);
+
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomId);
+            _logger.LogInformation("Participant {clientId} left the room {roomId}", Context.ConnectionId, roomId);
+            await RaiseRoomsUpdated();
         }
     }
 
@@ -83,7 +127,12 @@ public class ChannelHub : Hub
         using (await Mutex.LockAsync())
         {
             _logger.LogInformation("Raising rooms updated event");
-            await Clients.All.SendAsync("RoomsUpdated", Rooms);
+            await RaiseRoomsUpdated();
         }
+    }
+
+    private async Task RaiseRoomsUpdated()
+    {
+        await Clients.All.SendAsync("RoomsUpdated", Rooms);
     }
 }
